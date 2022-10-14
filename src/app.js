a02e89c8eb98371dffd069b0ba11b2b9f03eaa79
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2022-10-14 16:45:45 -0300

    features-convert-temp

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import { request } from "./app/request.js";
-import { urlAPIGeo, urlAPIWeather } from "./app/APIs.js";
+import { urlAPIGeo, urlAPIWeather, urlAPIWeatherFahr } from "./app/APIs.js";
 import { getDayWeek } from "./app/getDayWeek.js"; 
 import { splitInputUser } from "./app/DOM/splitInputUser.js";
 
@@ -10,6 +10,8 @@ import { updDayThreeDOM } from "./app/DOM/updDayThreeDOM.js";
 import { updDayFourDOM } from "./app/DOM/updDayFourDOM.js";
 
 const search = document.querySelector(".search");
+const bntFahr = document.getElementById("Fahrenheit");
+const btnCel = document.getElementById("Celsius");
 
 //Other days
 const dayOne = document.getElementById("dayOne");
@@ -18,6 +20,8 @@ const dayThree = document.getElementById("dayThree");
 const dayFour = document.getElementById("dayFour");
 
 const othersDays = [dayOne, dayTwo, dayThree, dayFour]
+
+let pstLocal ;
 
 search.addEventListener("keyup", (e) => {
   if(e.keyCode === 13){
@@ -34,11 +38,13 @@ search.addEventListener("keyup", (e) => {
         alert("Digite um valor coerente de local")
         search.textContent = ""
       }
+      pstLocal = positionLocal
 
       const {lat, lon} = positionLocal;
 
       const urlWeather = urlAPIWeather(lat, lon);
       const {list} = await request("GET", urlWeather);
+      // console.log(list);
 
       const {main, wind, weather} = list[0];
 
@@ -65,4 +71,64 @@ search.addEventListener("keyup", (e) => {
 
     APIAssignments();  
   };
+});
+
+bntFahr.addEventListener("click", () => {
+  const {lat, lon} = pstLocal;
+  async function rqstAPI(){
+    const urlFarh = urlAPIWeatherFahr(lat, lon);
+    const {list} = await request("GET", urlFarh);
+
+    const {main, wind, weather} = list[0];
+    handlerDayDOM(main, wind, weather);
+
+    for( let i = 1; i <= othersDays.length; i++){
+      let number = i * 8;
+      switch(number){
+        case 8:
+          const nameWeek = getDayWeek(list[number]);
+          updDayOneDOM(list[number], nameWeek, dayOne);
+        case 16:
+          const nameWeekTwo = getDayWeek(list[number]);
+          updDayTwoDOM(list[number],nameWeekTwo, dayTwo);
+        case 24:
+          const nameWeekThree = getDayWeek(list[number]);
+          updDayThreeDOM(list[number], nameWeekThree, dayThree);
+        case 32:
+          const nameWeekFour = getDayWeek(list[number]);
+          updDayFourDOM(list[number], nameWeekFour, dayFour);
+      }
+    }
+  }
+  rqstAPI();
+});
+
+btnCel.addEventListener("click", () => {
+  const {lat, lon} = pstLocal;
+  async function rqstAPI(){
+    const urlFarh = urlAPIWeather(lat, lon);
+    const {list} = await request("GET", urlFarh);
+
+    const {main, wind, weather} = list[0];
+    handlerDayDOM(main, wind, weather);
+
+    for( let i = 1; i <= othersDays.length; i++){
+      let number = i * 8;
+      switch(number){
+        case 8:
+          const nameWeek = getDayWeek(list[number]);
+          updDayOneDOM(list[number], nameWeek, dayOne);
+        case 16:
+          const nameWeekTwo = getDayWeek(list[number]);
+          updDayTwoDOM(list[number],nameWeekTwo, dayTwo);
+        case 24:
+          const nameWeekThree = getDayWeek(list[number]);
+          updDayThreeDOM(list[number], nameWeekThree, dayThree);
+        case 32:
+          const nameWeekFour = getDayWeek(list[number]);
+          updDayFourDOM(list[number], nameWeekFour, dayFour);
+      }
+    }
+  }
+  rqstAPI();
 });
--- src/app/APIs.js
+++ src/app/APIs.js
@@ -1,4 +1,4 @@
-const key = "0c83da6e21a88bbbd8d6121eaa0e7d1f";
+const key = "edd6fe0ccc700d85efea6c1e00032c6d";
 
 export function urlAPIGeo(city, state, country){
   const url =  `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&appid=${key}`;
@@ -8,4 +8,9 @@ export function urlAPIGeo(city, state, country){
 export function urlAPIWeather(lat, lon){
   const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=metric`;
   return url;
+}
+
+export function urlAPIWeatherFahr(lat, lon){
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`;
+  return url
 }
